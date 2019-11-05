7d92dd59eba9fb02d0f913ef9110b0c673875445
Serhii Shved <chuhaystyr@gmail.com>
2019-11-05 07:54:02 +0200

    debounce

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-let searchTemplate = document.getElementById('searchTemplate')
-let inputText = document.getElementById('inputText') 
-let totalResult = document.getElementById('totalResult')
+let searchTemplate = document.getElementById("searchTemplate")
+let inputText = document.getElementById("inputText")
+let totalResult = document.getElementById("totalResult")
 
-let template = '' 
-let input = ''
+let template = ""
+let input = ""
 let result = 0
 
 searchTemplate.oninput = function() {
-    template = searchTemplate.value;
+  template = searchTemplate.value
 }
-
-
-inputText.oninput = function() {
-    input = inputText.value;
-    result = search(template, input);
-    totalResult.innerHTML = result;
-}
-
 
 function search(template, text) {
-    let pos = -1;
-    let result = 0;
-    while ((pos = text.indexOf(template, pos + 1)) != -1) {
-        result += 1;
-    }
-    return result;
+  let count = 0
+  let pos = -1
+  while ((pos = text.indexOf(template, pos + 1)) != -1) {
+    count++
+  }
+  result = count
+}
+
+function debounce(f, ms) {
+  let isCooldown = false
+  return function() {
+    if (isCooldown) return
+    f.apply(this, arguments)
+    isCooldown = true
+    setTimeout(() => (isCooldown = false), ms)
+  }
+}
+
+search = debounce(search, 3000)
+
+inputText.oninput = function() {
+  input = inputText.value
+  search(template, input)
+  totalResult.innerHTML = result
 }
