68adbeed689014d336ffdf8bcf28b7074bec4e5c
Serhii Shved <chuhaystyr@gmail.com>
2019-11-06 23:49:29 +0200

    debounce

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
-let searchTemplate = document.getElementById("searchTemplate")
-let inputText = document.getElementById("inputText")
-let totalResult = document.getElementById("totalResult")
+;(function() {
+  let searchTemplate = document.getElementById("searchTemplate")
+  let inputText = document.getElementById("inputText")
+  let totalResult = document.getElementById("totalResult")
 
-let template = ""
-let input = ""
-let result = 0
+  let template = ""
+  let input = ""
+  let result = 0
 
-searchTemplate.oninput = function() {
-  template = searchTemplate.value
-}
-
-function search(template, text) {
-  let count = 0
-  let pos = -1
-  while ((pos = text.indexOf(template, pos + 1)) != -1) {
-    count++
+  searchTemplate.oninput = function() {
+    template = searchTemplate.value
   }
-  result = count
-}
 
-function debounce(f, ms) {
-  let isCooldown = false
-  return function() {
-    if (isCooldown) {
-      return
+  function search(template, text) {
+    let count = 0
+    let pos = -1
+    while ((pos = text.indexOf(template, pos + 1)) != -1) {
+      count++
     }
-    f.apply(this, arguments)
-    isCooldown = true
-    setTimeout(() => (isCooldown = false), ms)
+    result = count
   }
-}
 
-search = debounce(search, 3000)
+  function debounce(f, ms) {
+    let isCooldown = false
+    return function() {
+      if (isCooldown) {
+        return
+      }
+      f.apply(this, arguments)
+      isCooldown = true
+      setTimeout(() => (isCooldown = false), ms)
+    }
+  }
 
-inputText.oninput = function() {
-  input = inputText.value
-  search(template, input)
-  totalResult.innerHTML = result
-}
+  search = debounce(search, 3000)
+
+  inputText.oninput = function() {
+    input = inputText.value
+    search(template, input)
+    totalResult.innerHTML = result
+  }
+})()
